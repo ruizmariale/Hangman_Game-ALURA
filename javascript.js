96c13faed5baa16a7f98e8199df108ce93508faf
ruizmariale <ruizdiazm2906@gmail.com>
2022-11-05 19:18:21 -0300

    Subiendo archivos al repositorio Rev1.0

---
+++ javascript.js
@@ -0,0 +1,162 @@
+const correcto = document.querySelector("#correcto");
+const incorrecto = document.querySelector("#incorrecto");
+const nuevaPalabra = document.querySelector("#nuevaPalabra");
+let palabras = ["ALURA", "ALTA", "ABILITY", "ACCOUNT", "ADVANCE", "AFTERNOON", "AGREE", "ALMOST", "ANALYSIS", "ANYTHING", "BALANCE", "BEAUTIFUL", "BELIEVE", "BUSINESS", "CHALLENGE", "COMPUTER", "CONTAIN", "CULTURE", "CUSTOMER", "DAUGHTER", "DEGREE", "DEVELOPMENT", "DOCUMENT", "DRAW", "EDUCATION", "EMPLOYEE", "ENVIRONMENT", "FAMILY", "FINISH", "FOCUS", "FORWARD", "FUNCTION", "FOUNDATION", "GENERATION", "GUESS", "HEALTH", "HOLIDAY", "IDENTIFY", "IMPROVE", "INTERVIEW", "KEEP", "LANGUAGE", "LEADER", "LEVEL", "LOSE", "MAGAZINE", "MANAGER", "MEAN", "MEMORY", "NETWORK", "OBJECT", "PARTY", "POPULAR", "POSSIBILITY", "PRODUCTO", "ORACLE"];
+let secreto = "";
+let ltrCorrecta = [];
+let ltrIncorrecta = [];
+
+function finalJuego(ganaste) {
+  document.removeEventListener("keypress", logKey);
+  if (ganaste) {
+    limpiarTablero();
+    dibujarHombre(150, 50, "#f66b40", 9);
+  }
+}
+function dibujarPalabra() {
+  var $elem = correcto;
+  let conteo = 0;
+  $elem.innerHTML = "";
+  for (let ltr of secreto) {
+    let $span = document.createElement("span");
+    let $txt = document.createTextNode("");
+    if (ltrCorrecta.indexOf(ltr) >= 0) {
+      $txt.nodeValue = ltr;
+      conteo++;
+    }
+    $span.setAttribute("class", "letra correcta");
+    $span.appendChild($txt);
+    $elem.appendChild($span);
+  }
+  if (conteo == secreto.length) {
+    finalJuego(true);
+  }
+}
+function dibujarLetras() {
+  var $elem = incorrecto;
+  let conteo = 9;
+  $elem.innerHTML = "";
+  for (let ltr of ltrIncorrecta) {
+    let $span = document.createElement("span");
+    let $txt = document.createTextNode(ltr);
+    $span.setAttribute("class", "letra incorrecta");
+    $span.appendChild($txt);
+    $elem.appendChild($span);
+    conteo--;
+    if (conteo > 5) {
+      dibujarPoste(88, conteo);
+    } else {
+      dibujarHombre(88 + 110, 37, "#000c0c", conteo);
+    }
+  }
+  if (conteo == 0) {
+    finalJuego(false);
+  }
+}
+function buscar(letra) {
+  if (secreto.includes(letra)) {
+    if (ltrCorrecta.indexOf(letra) < 0) {
+      ltrCorrecta.push(letra);
+      dibujarPalabra();
+    }
+  } else {
+    if (ltrIncorrecta.indexOf(letra) < 0) {
+      ltrIncorrecta.push(letra);
+      dibujarLetras();
+    }
+  }
+}
+function logKey(tecla) {
+  var caracter = tecla.key.toUpperCase();
+  if (caracter != caracter.toLowerCase() && caracter.length == 1) {
+    buscar(caracter);
+  }
+}
+function palabraSecreta() {
+  let alea = Math.round((palabras.length - 1) * Math.random());
+  return palabras[alea];
+}
+function juego() {
+  ltrCorrecta = [];
+  ltrIncorrecta = [];
+  secreto = palabraSecreta();
+  limpiarTablero();
+  dibujarPalabra();
+  incorrecto.innerHTML = "";
+  document.addEventListener("keypress", logKey);
+}
+function mostrar(elementoID) {
+  document.getElementById(elementoID).style.display = "block";
+}
+function ocultar(elementoID) {
+  document.getElementById(elementoID).style.display = "none";
+}
+function prepPalabra() {
+  const noPermitido = /[^A-ZÑ]/g;
+  let texto = nuevaPalabra.value.trim().toUpperCase();
+  noPermitido.lastIndex = 0;
+  if (noPermitido.test(texto)) {
+    //Separa y elimina los acentos diacriticos del string (excepto "ñ")
+    texto = texto
+      .normalize("NFD")
+      .replace(/[\u0300-\u0302,\u0304-\u036f]/g, "");
+    texto = texto.normalize().replace(noPermitido, "");
+  }
+  return texto;
+}
+function inputVerificar() {
+  nuevaPalabra.addEventListener("keypress", function (tecla) {
+    var caracter = tecla.key.toUpperCase();
+    if (!(caracter != caracter.toLowerCase() && caracter.length == 1)) {
+      tecla.preventDefault();
+    }
+  });
+}
+
+// Seccion de funciones activadas por "onClick" en los botones.
+function iniciar() {
+  ocultar("div-agregar");
+  ocultar("home-button");
+  mostrar("div-juego");
+  
+  juego();
+}
+function nuevo() {
+  document.getElementById("bot-nuevo").blur();
+  juego();
+}
+function desistir() {
+  secreto = "";
+  ocultar("div-juego");
+  mostrar("home-button");
+}
+function agregar() {
+  mostrar("div-agregar");
+  nuevaPalabra.value = "";
+  nuevaPalabra.focus();
+  inputVerificar();
+}
+function newWord() {
+  let palabra = prepPalabra();
+  let cantLetras = palabra.length;
+  if (cantLetras < 1 || cantLetras > 10) {
+    alert("¡The entered word is not valid!");
+  } else {
+    if (palabras.indexOf(palabra) < 0) {
+      palabras.push(palabra);
+      alert("The word " + palabra + " was add to the list!");
+      nuevaPalabra.value = "";
+      nuevaPalabra.focus();
+    } else {
+      alert("The word " + palabra + " was already added!");
+    }
+    console.log(palabras);
+  }
+}
+function revelar() {
+  if (secreto != "") {
+    alert("The secret word is " + secreto + ".");
+  } else {
+    alert("Shows the secret word when the game starts.");
+  }
+}
